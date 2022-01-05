0814a068eed0fb778d16a368259624b17661f232
koldin108902 <alex108902@naver.com>
2022-01-05 15:52:51 +0900

    function and variable

--- hello-world/src/main/kotlin/helloWorld.kt
+++ hello-world/src/main/kotlin/helloWorld.kt
@@ -3,4 +3,22 @@ class HelloWorld {
 
 fun main(args: Array<String>) {
     println("Hello World!")
+    println(sum(1, 1))
+    println(stringTemp())
+}
+
+fun sum(a: Int, b: Int): Int {
+    val c = 2
+    val d: Int = 3
+    println(c + d)
+    return a + b
+}
+
+fun sum2(a: Int, b: Int) = a + b
+
+fun stringTemp(): String {
+    var a = 1
+    val s = "Hello World! $a"
+
+    return "Hello World! ${s.replace("World", "kotlin")}"
 }
